6bdaf01ab00d282a7e1dd1209ffad8779717070d
Mansour <mansourlol440@gmail.com>
2024-10-30 18:54:57 +0000

    I lost my charger

--- frontend/src/components/profileBar/ProfileBar.jsx
+++ frontend/src/components/profileBar/ProfileBar.jsx
@@ -5,13 +5,15 @@ import "./ProfileBar.scss";
 export default function ProfileBar() {
   return (
     <header id="container-profile-bar">
-      <div>
+      <div className="container-profile-bar-input">
+        <div className="container-info-profile-name">
         <img src="/images/icons/magnifying-glass.svg" alt="Img magnifying glass" />
         <p> Profile Name </p>
-      </div>
+        </div>
         <Link id="icon-user" to="/account">
           <img src="/images/icons/user.svg" alt="" />
         </Link>
+      </div>
     </header>
   );
 }
