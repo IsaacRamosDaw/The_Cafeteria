2d68a1511bcb73ae1bcfa37df44c642ac02a759f
Mansour <mansourlol440@gmail.com>
2024-10-30 18:51:07 +0000

    Profile bar in admin home

--- frontend/src/components/profileBar/ProfileBar.jsx
+++ frontend/src/components/profileBar/ProfileBar.jsx
@@ -4,13 +4,14 @@ import "./ProfileBar.scss";
 
 export default function ProfileBar() {
   return (
-    <nav id="container-profile-bar">
+    <header id="container-profile-bar">
       <div>
-        
+        <img src="/images/icons/magnifying-glass.svg" alt="Img magnifying glass" />
+        <p> Profile Name </p>
+      </div>
         <Link id="icon-user" to="/account">
           <img src="/images/icons/user.svg" alt="" />
         </Link>
-      </div>
-    </nav>
+    </header>
   );
 }
--- frontend/src/pages/adminHome/AdminHome.jsx
+++ frontend/src/pages/adminHome/AdminHome.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ProfileBar from "../../components/profileBar/ProfileBar";
 import "./AdminHome.scss";
-import AdminContainer from '../../components/adminComponents/adminContainer/AdminContainer'
+import AdminContainer from "../../components/adminComponents/adminContainer/AdminContainer";
 import SchoolContainer from "../../components/adminComponents/schoolsContainer/SchoolContainers";
-import WorkerContainer from '../../components/adminComponents/workerContainer/WorkerContainer'
+import WorkerContainer from "../../components/adminComponents/workerContainer/WorkerContainer";
 function AdminHome() {
   return (
-    <main id="admin-home">
-      <ProfileBar/>
-      <SchoolContainer />
-      <AdminContainer />
-      <WorkerContainer />
+    <main id="container-admin-home">
+      <ProfileBar />
+      <div className="main-content">
+        <SchoolContainer />
+        <AdminContainer />
+        <WorkerContainer />
+      </div>
     </main>
   );
 }
