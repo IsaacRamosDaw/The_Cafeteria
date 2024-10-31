448b31fe86edf58371d2595451b8edbfd6b5e484
Mansour <mansourlol440@gmail.com>
2024-10-31 17:03:02 +0000

    Fix db name

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,11 +2,7 @@ module.exports = {
   HOST: "localhost",
   USER: "root",
   PASSWORD: "1234",
-<<<<<<< HEAD
-  DB: "db_cafeteria",
-=======
   DB: "db_coffeshop",
->>>>>>> 36788805c83813b6ce933c95a478b69c73c9ed6a
   dialect: "mysql",
   pool: {
     max: 5,
