bdcd20ebfa8180a5f3a584bbc4a18be1b6885a65
Isaac Ramos <isaacantonioramosperez@alumno.ieselrincon.es>
2024-10-31 15:40:55 +0000

    preparando los fetchs

--- frontend/src/pages/admin/forms/AdminForm.jsx
+++ frontend/src/pages/admin/forms/AdminForm.jsx
@@ -1,28 +1,64 @@
 import React from "react";
+import Button from "../../../components/button/Button";
 import "./Form.scss";
-
 import Label from "../../../components/label/Label";
 
 function AdminForm() {
+  const handleCreate = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(document.getElementById("admin-form"));
+
+    try {
+      const response = await fetch("http://localhost:8080/api/admin", {
+        method: "POST",
+        body: formData,
+      });
+      console.log(await response.json());
+    } catch (error) {
+      console.error("Error al crear:", error);
+    }
+  };
+
+  const handleEdit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(document.getElementById("admin-form"));
+
+    try {
+      const response = await fetch("/api/admin", {
+        method: "PUT",
+        body: formData,
+      });
+
+      console.log(await response.json());
+    } catch (error) {
+      console.error("Error al editar:", error);
+    }
+  };
+
   return (
     <main className="form-container">
       <form id="admin-form">
+        <h2>Admin</h2>
         <Label
           id={"name-admin"}
-          placeHolder={"Name of the admin"}
-          title={"Name"}
+          placeHolder={"Nombre del administrador"}
+          title={"Nombre"}
           type={"text"}
         />
         <Label
           id={"password-admin"}
-          placeHolder={"password of the admin"}
-          title={"Password"}
+          placeHolder={"Contraseña del administrador"}
+          title={"Contraseña"}
           type={"password"}
         />
+
+        <div id="buttons">
+          <Button onClick={handleCreate} text={"Crear"} />
+          <Button onClick={handleEdit} text={"Editar"} />
+        </div>
       </form>
     </main>
   );
 }
-
 
 export default AdminForm;
--- frontend/src/pages/admin/forms/SchoolForm.jsx
+++ frontend/src/pages/admin/forms/SchoolForm.jsx
@@ -3,9 +3,40 @@ import "./Form.scss";
 import Label from "../../../components/label/Label";
 
 function SchoolForm() {
+  const handleCreate = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(document.getElementById("school-form"));
+
+    try {
+      const response = await fetch("http://localhost:8080/api/school", {
+        method: "POST",
+        body: formData,
+      });
+      console.log(await response.json());
+    } catch (error) {
+      console.error("Error al crear:", error);
+    }
+  };
+
+  const handleEdit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(document.getElementById("worker-form"));
+
+    try {
+      const response = await fetch("/api/worker", {
+        method: "PUT",
+        body: formData,
+      });
+
+      console.log(await response.json());
+    } catch (error) {
+      console.error("Error al editar:", error);
+    }
+  };
   return (
     <main className="form-container">
       <form id="school-form">
+        <h2>Schools</h2>
         <Label
           id={"name-school"}
           placeHolder={"Name of the school"}
@@ -21,9 +52,19 @@ function SchoolForm() {
         <Label
           id={"phone-school"}
           placeHolder={"phone of the school"}
-          title={"Name"}
+          title={"Phone"}
+          type={"number"}
+        />
+        <Label
+          id={"phone-school"}
+          placeHolder={"Adress of the school"}
+          title={"Adress"}
           type={"text"}
         />
+        <div id="buttons">
+          <Button onClick={handleCreate} text={"Crear"} />
+          <Button onClick={handleEdit} text={"Editar"} />
+        </div>
       </form>
     </main>
   );
