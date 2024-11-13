6bcc189d0a60857c5dfd0856667c1ac577cf676f
Mansour <mansourlol440@gmail.com>
2024-11-13 18:46:15 +0000

    Orders page fixed

--- frontend/src/pages/orders/Orders.jsx
+++ frontend/src/pages/orders/Orders.jsx
@@ -5,19 +5,20 @@ import Order from "../../components/order/Order";
 import './Orders.scss';
 
 function Orders(){
+
+    // Crear un array de estado para guardar los pedidos y que se mapee para mostrarlos
+
     return(
-        <>
+        <div id="Orders-page">
             <SearchBar />
             <Separator/>
             <main id="orders-container">
-                <h1>Orders</h1>
-                <Order/>    
-                <Order/>    
-                <Order/>    
-                <Order/>    
+                <Order ID_order={"000000"} date={"10/12/2020"} product={"Super Bocata Combo"} quantity={"x1"} />    
+                <Order ID_order={"000000"} studentName={"Isaac DAWT"} date={"10/12/2020"} product={"Super Bocata Combo"} quantity={"x1"} />    
+                <Order ID_order={"000000"} date={"10/12/2020"} product={"Super Bocata Combo"} quantity={"x1"} />    
             </main>
             <TabsBar />
-        </>
+        </div>
     )
 }
 
