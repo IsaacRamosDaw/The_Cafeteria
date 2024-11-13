35cb82652125ab988c6a71a0f6b68ad0e0308b8c
IsaacRamosDaw <isaacantonioramosperez@alumno.ieselrincon.es>
2024-11-13 02:17:17 +0100

    settings in progress 2

--- frontend/src/components/setttingsComp/Setting.jsx
+++ frontend/src/components/setttingsComp/Setting.jsx
@@ -1,14 +1,51 @@
+import React, { useState, useEffect, useRef } from "react";
 import "./Setting.scss";
-import { Link } from "react-router-dom";
-function Setting({ icon, title, to }) {
+
+function Setting({ title, text, placeholder, type = 'text' }) {
+  const [isFormVisible, setFormVisible] = useState(false);
+  const formRef = useRef(null);
+
+  const handleClick = () => {
+    setFormVisible(!isFormVisible);
+  };
+
+    const handleOutsideClick = (e) => {
+      if (formRef.current && !formRef.current.contains(e.target)) {
+        setFormVisible(false);
+      }
+  };
+  
+    const handleFormSubmit = (e) => {
+      e.preventDefault(); // Evita el comportamiento de recarga de página por defecto
+      // Aquí puedes añadir la lógica de envío de datos, por ejemplo:
+      console.log("Formulario enviado");
+      setFormVisible(false); // Oculta el formulario después del envío
+    };
+
+  useEffect(() => {
+    if (isFormVisible) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isFormVisible]);
+
   return (
-    <Link to={""}>
-      <div id="setting">
-        <img src={icon} />
-        <p>{title}</p>
-        <img src="/images/icons/arrow.svg" alt="" />
-      </div>
-    </Link>
+    <div id="setting">
+      <p onClick={handleClick}>{title}</p>
+      <form
+        id="modal"
+        ref={formRef}
+        className={isFormVisible ? "visible" : ""}
+        onSubmit={handleFormSubmit}
+      >
+        <h3>{text}</h3>
+        <input type={type} id="inpu" placeholder={placeholder} />
+      </form>
+    </div>
   );
 }
 
--- frontend/src/pages/settings/studentSettings/StudentSettings.jsx
+++ frontend/src/pages/settings/studentSettings/StudentSettings.jsx
@@ -1,9 +1,10 @@
 import SearchBar from "../../../components/searchBar/SearchBar"
-import Setting from "../../../components/setttingsComp/Setting"
 import TabsBar from "../../../components/tabsBar/tabsBar"
 import Separator from "../../../components/separator/Separator"
+import { Link } from "react-router-dom";
 
 import './StudentSettings.scss'
+import Setting from "../../../components/setttingsComp/Setting";
 function StudentSettings() {
   
   return (
@@ -11,22 +12,43 @@ function StudentSettings() {
       <SearchBar />
       <Separator />
       <main id="student-setttings">
-        <div>
+        <div id="wallet-container">
           <div id="wallet">
             <p>Créditos restantes</p>
-            <p id="money">$dinero</p>
+            <p id="money">37€</p>
           </div>
-          <Setting
-            title={"Perfil"}
-            icon={"../../../../public/images/icons/profile.svg"}
-            to={""}
-          />
-          <Setting
-            title={"Creditos"}
-            icon={"../../../../public/images/icons/credits.svg"}
-            to={""}
-          />
+          <div id="add-money">
+            <Link>
+              <img src="/public/images/icons/plus.svg" alt="add your money" />
+            </Link>
+          </div>
         </div>
+        <form id="delete">
+          <h2>Delete account</h2>
+        </form>
+        
+        <Setting
+          title={"Nombre"}
+          text={"Escoge tu nuevo nombre"}
+          placeholder={"último nombre"}
+        />
+        <Setting
+          title={"Email"}
+          text={"Escoge tu nuevo Email"}
+          placeholder={"Último Email"}
+        />
+        <Setting
+          title={"Teléfono"}
+          text={"Escoge tu nuevo Teléfono"}
+          type="number"
+          placeholder={"Último Email"}
+        />
+        <Setting
+          title={"Password"}
+          text={"Escoge tu nueva contraseña"}
+          type="password"
+          placeholder={"Último Email"}
+          />
       </main>
       <TabsBar />
     </>
