9a9e32ac5d916716e0472adc002f02286292e072
Cynthia2221 <cynthiasanchezher@gmail.com>
2024-11-15 18:59:49 +0000

    Backend unfinished

--- frontend/src/services/adminService.js
+++ frontend/src/services/adminService.js
@@ -1,50 +1,5 @@
 const endpoint = "http://localhost:8080/api/admin";
 
-// export async function login(username, password) {
-//   try {
-//     const response = await fetch(`${endpoint}/signin`, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ username, password }),
-//     });
-
-//     if (!response.ok) {
-//       throw new Error("Login failed");
-//     }
-
-//     const data = await response.json();
-//     localStorage.setItem("token", data.token); // Save token
-//     return data;
-//   } catch (error) {
-//     console.error("Login error:", error);
-//     throw error;
-//   }
-// }
-
-// function getAuthHeaders() {
-//   const token = localStorage.getItem("token");
-//   return token ? { Authorization: `Bearer ${token}` } : {};
-// }
-
-
-// export function get() {
-//   return fetch(endpoint, {
-//     headers: getAuthHeaders(),
-//   })
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Error fetching data");
-//       }
-//       return response.json();
-//     })
-//     .catch((error) => {
-//       console.log(`error, ${error}`);
-//       return error;
-//     });
-// }
-
 export function get() {
   const getOperation = fetch(endpoint)
     .then((response) => {
@@ -59,22 +14,6 @@ export function get() {
     });
   return getOperation;
 }
-
-// export function getOne(id) {
-//   return fetch(`${endpoint}/${id}`, {
-//     headers: getAuthHeaders(),
-//   })
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Error fetching data");
-//       }
-//       return response.json();
-//     })
-//     .catch((error) => {
-//       console.log(`error, ${error}`);
-//       return error;
-//     });
-// }
 
 export function getOne(id) {
   const getOneOperation = fetch(`${endpoint}/${id}`)
@@ -95,9 +34,9 @@ export async function create(formData) {
   return fetch(endpoint, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: JSON.stringify(formData),
+    body: new URLSearchParams(formData),
   })
     .then((response) => {
       if (!response.ok) {
@@ -115,7 +54,6 @@ export async function create(formData) {
 export async function remove(id) {
   const removeOperation = fetch(`${endpoint}/${id}`, {
     method: "DELETE",
-    // headers: getAuthHeaders(),
   })
     .then((response) => {
       if (!response.ok) {
@@ -137,7 +75,6 @@ export async function edit(id, data) {
   return fetch(url, {
     method: "PUT",
     headers: {
-      // ...getAuthHeaders(),
       "Content-Type": "application/x-www-form-urlencoded",
     },
     body: new URLSearchParams(data),
