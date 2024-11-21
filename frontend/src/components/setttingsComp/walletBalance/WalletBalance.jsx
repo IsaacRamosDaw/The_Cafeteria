652de3268a679cd972eba952b86cc1b787cd178d
Mansour <mansourlol440@gmail.com>
2024-11-21 17:10:28 +0000

    Last commit before changing to default settings

--- frontend/src/components/setttingsComp/walletBalance/WalletBalance.jsx
+++ frontend/src/components/setttingsComp/walletBalance/WalletBalance.jsx
@@ -14,32 +14,33 @@ import { useState } from "react";
 import InputFormSetting from "../../../components/setttingsComp/inputFormSetting/InputFormSetting";
 
 export default function WalletBalance() {
+  const [isCard, setIsCard] = useState(false);
+
   const [card, setCard] = useState({
-    number: `${0}`,
+    number: `${5}`,
     date: `-`,
     cvc: `${0}`,
     balance: 0,
   });
 
+  // Add money, card and details handlers
   const [openMoney, setOpenMoney] = useState(false);
-  const handleOpenMoney = () => setOpenMoney(true);
-  const handleCloseMoney = () => setOpenMoney(false);
+  const handleAddMoney = () => setOpenMoney(openMoney ? false : true);
 
   const [openDetails, setOpenDetails] = useState(false);
-  const handleOpenDetails = () => setOpenDetails(true);
-  const handleCloseDetails = () => setOpenDetails(false);
+  const handleCardDetails = () => setOpenDetails(openDetails ? false : true);
 
-  const handleAddMoney = (e) => {
+  const [openAddCard, setOpenAddCard] = useState(false);
+  const handleAddCard = () => setOpenAddCard(openAddCard ? false : true);
+
+  // Forms handlers
+  const handleFormAddMoney = (e) => {
     e.preventDefault();
 
     console.log(e.target);
   };
 
-  const [openAddCard, setOpenAddCard] = useState(false);
-  const handleOpenAddCard = () => setOpenAddCard(true);
-  const handleCloseAddCard = () => setOpenAddCard(false);
-
-  const handleAddCard = (e) => {
+  const handleFormAddCard = (e) => {
     e.preventDefault();
 
     const numberCard = document.getElementById("numero");
@@ -48,32 +49,26 @@ export default function WalletBalance() {
 
     console.log(numberCard, dateCard, cvcCard);
 
-    setCard(
-      {
+    setCard({
       number: numberCard,
       date: dateCard,
       cvc: cvcCard,
-    }
-  );
-
-    handleCloseAddCard();
+    });
   };
 
-  const handleRemoveCard = () => {
-
+  const handleFormRemoveCard = () => {
+    setIsCard(false);
   };
 
   return (
     <section id="wallet-balance-container">
       <div id="container-balance">
         <p className="balance-title">Your balance</p>
-        <h1 className="amount-balance"> €{card.balance} </h1>
+        <h1 className="amount-balance"> €{isCard ? card.number : 0} </h1>
       </div>
       <div id="container-card-credit">
         <div id="carusel-credit-card">
-          {card.number == 0 ? (
-            ""
-          ) : (
+          {isCard ? (
             <div className="credit-card">
               {card.number[0] == 4 ? (
                 <RiVisaLine />
@@ -83,23 +78,19 @@ export default function WalletBalance() {
                 ""
               )}
             </div>
-          )}
-
-          {card.number != 0 ? (
-            ""
           ) : (
-            <div onClick={handleOpenAddCard} id="credit-card-add">
+            <div onClick={handleAddCard} id="credit-card-add">
               <IoIosAddCircle />
               <h3 className="text-add-card">Añade tu tarjeta</h3>
             </div>
           )}
 
-          <Modal open={openAddCard} onClose={handleCloseAddCard}>
+          <Modal open={openAddCard} onClose={handleAddCard}>
             <Box className="modal-container">
               <div className="container-exit-icon">
-                <IoClose className="icon-exit" onClick={handleCloseAddCard} />
+                <IoClose className="icon-exit" onClick={handleAddCard} />
               </div>
-              <form onSubmit={handleAddCard} className="content-modal">
+              <form onSubmit={handleFormAddCard} className="content-modal">
                 <InputFormSetting
                   title={"Numero de tarjeta"}
                   placeholder={card.number}
@@ -129,20 +120,20 @@ export default function WalletBalance() {
             </Box>
           </Modal>
         </div>
-        <h2>{card.number == 0 ? "- - - -" : card.number}</h2>
+        <h2>{isCard ? card.number : "- - - -"}</h2>
       </div>
 
       <div id="card-options">
-        <div className="option-card add" onClick={handleOpenMoney}>
+        <div className="option-card add" onClick={handleAddMoney}>
           <IoMdAddCircleOutline className="card-icon-option" />
           <h2>Add Money</h2>
         </div>
-        <Modal open={openMoney} onClose={handleCloseMoney}>
+        <Modal open={openMoney} onClose={handleAddMoney}>
           <Box className="modal-container">
             <div className="container-exit-icon">
-              <IoClose className="icon-exit" onClick={handleCloseMoney} />
+              <IoClose className="icon-exit" onClick={handleAddMoney} />
             </div>
-            <form onSubmit={handleAddMoney} className="content-modal">
+            <form onSubmit={handleFormAddMoney} className="content-modal">
               <InputFormSetting
                 title={"Cuenta"}
                 placeholder={card.number}
@@ -162,14 +153,14 @@ export default function WalletBalance() {
           </Box>
         </Modal>
 
-        <div className="option-card details" onClick={handleOpenDetails}>
+        <div className="option-card details" onClick={ isCard ? handleCardDetails : ""}>
           <MdCreditCard className="card-icon-option" />
           <h2>Card Details</h2>
         </div>
-        <Modal open={openDetails} onClose={handleCloseDetails}>
+        <Modal open={openDetails} onClose={handleCardDetails}>
           <Box className="modal-container">
             <div className="container-exit-icon">
-              <IoClose className="icon-exit" onClick={handleCloseDetails} />
+              <IoClose className="icon-exit" onClick={handleCardDetails} />
             </div>
             <div className="content-modal">
               <InputFormSetting
@@ -194,7 +185,7 @@ export default function WalletBalance() {
           </Box>
         </Modal>
 
-        <div onClick={handleRemoveCard} className="option-card">
+        <div onClick={handleFormRemoveCard} className="option-card">
           <MdOutlineRemoveCircleOutline className="card-icon-option trash" />
           <h2>Remove Card</h2>
         </div>
