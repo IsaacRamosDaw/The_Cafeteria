332900ce4895b329aa0c4de0b7dea423d94c6cca
Mansour <mansourlol440@gmail.com>
2024-11-26 18:36:30 +0000

    Minor fixes and ready for updata settings account

--- frontend/src/components/setttingsComp/walletBalance/WalletBalance.jsx
+++ frontend/src/components/setttingsComp/walletBalance/WalletBalance.jsx
@@ -2,90 +2,28 @@ import "./WalletBalance.scss";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 
-import { MdCreditCard } from "react-icons/md";
-import { MdOutlineRemoveCircleOutline } from "react-icons/md";
 import { IoMdAddCircleOutline } from "react-icons/io";
-import { IoIosAddCircle } from "react-icons/io";
-import { IoClose } from "react-icons/io5";
 
-import { RiMastercardFill } from "react-icons/ri";
-import { RiVisaLine } from "react-icons/ri";
 import { useState } from "react";
-import InputFormSetting from "../../../components/setttingsComp/inputFormSetting/InputFormSetting";
 
 export default function WalletBalance() {
-  const [isCard, setIsCard] = useState(false);
-
-  const [card, setCard] = useState({
-    number: `${5}`,
-    date: `-`,
-    cvc: `${0}`,
-    balance: 0,
-  });
 
   // Add money, card and details handlers
   const [openMoney, setOpenMoney] = useState(false);
   const handleAddMoney = () => setOpenMoney(openMoney ? false : true);
 
-  const [openDetails, setOpenDetails] = useState(false);
-  const handleCardDetails = () => setOpenDetails(openDetails ? false : true);
-
-  const [openAddCard, setOpenAddCard] = useState(false);
-  const handleAddCard = () => setOpenAddCard(openAddCard ? false : true);
-
-  // Forms handlers
-  const handleFormAddMoney = (e) => {
-    e.preventDefault();
-
-    console.log(e.target);
-  };
-
-  const handleFormAddCard = (e) => {
-    e.preventDefault();
-
-    const numberCard = document.getElementById("numero");
-    const dateCard = document.getElementById("fecha");
-    const cvcCard = document.getElementById("cvc");
-
-    console.log(numberCard, dateCard, cvcCard);
-
-    setCard({
-      number: numberCard,
-      date: dateCard,
-      cvc: cvcCard,
-    });
-  };
-
-  const handleFormRemoveCard = () => {
-    setIsCard(false);
-  };
-
+  
   return (
     <section id="wallet-balance-container">
       <div id="container-balance">
         <p className="balance-title">Your balance</p>
-        <h1 className="amount-balance"> €{isCard ? card.number : 0} </h1>
+        <h1 className="amount-balance"> € </h1>
       </div>
       <div id="container-card-credit">
         <div id="carusel-credit-card">
-          {isCard ? (
-            <div className="credit-card">
-              {card.number[0] == 4 ? (
-                <RiVisaLine />
-              ) : card.number[0] == 5 ? (
-                <RiMastercardFill />
-              ) : (
-                ""
-              )}
-            </div>
-          ) : (
-            <div onClick={handleAddCard} id="credit-card-add">
-              <IoIosAddCircle />
-              <h3 className="text-add-card">Añade tu tarjeta</h3>
-            </div>
-          )}
+          
 
-          <Modal open={openAddCard} onClose={handleAddCard}>
+          {/* <Modal open={openAddCard} onClose={handleAddCard}>
             <Box className="modal-container">
               <div className="container-exit-icon">
                 <IoClose className="icon-exit" onClick={handleAddCard} />
@@ -118,9 +56,8 @@ export default function WalletBalance() {
                 </button>
               </form>
             </Box>
-          </Modal>
+          </Modal> */}
         </div>
-        <h2>{isCard ? card.number : "- - - -"}</h2>
       </div>
 
       <div id="card-options">
@@ -128,7 +65,7 @@ export default function WalletBalance() {
           <IoMdAddCircleOutline className="card-icon-option" />
           <h2>Add Money</h2>
         </div>
-        <Modal open={openMoney} onClose={handleAddMoney}>
+        {/* <Modal open={openMoney} onClose={handleAddMoney}>
           <Box className="modal-container">
             <div className="container-exit-icon">
               <IoClose className="icon-exit" onClick={handleAddMoney} />
@@ -151,44 +88,7 @@ export default function WalletBalance() {
               </div>
             </form>
           </Box>
-        </Modal>
-
-        <div className="option-card details" onClick={ isCard ? handleCardDetails : ""}>
-          <MdCreditCard className="card-icon-option" />
-          <h2>Card Details</h2>
-        </div>
-        <Modal open={openDetails} onClose={handleCardDetails}>
-          <Box className="modal-container">
-            <div className="container-exit-icon">
-              <IoClose className="icon-exit" onClick={handleCardDetails} />
-            </div>
-            <div className="content-modal">
-              <InputFormSetting
-                title={"Numero de tarjeta"}
-                placeholder={card.number}
-                disable={true}
-                type={"number"}
-              />
-              <InputFormSetting
-                title={"Fecha de caducidad"}
-                placeholder={card.date}
-                disable={true}
-                type={"text"}
-              />
-              <InputFormSetting
-                title={"CVC"}
-                placeholder={card.cvc}
-                disable={true}
-                type={"number"}
-              />
-            </div>
-          </Box>
-        </Modal>
-
-        <div onClick={handleFormRemoveCard} className="option-card">
-          <MdOutlineRemoveCircleOutline className="card-icon-option trash" />
-          <h2>Remove Card</h2>
-        </div>
+        </Modal> */}
       </div>
     </section>
   );
--- frontend/src/pages/settings/studentSettings/accountSettings/AccountSettings.jsx
+++ frontend/src/pages/settings/studentSettings/accountSettings/AccountSettings.jsx
@@ -1,9 +1,83 @@
+import SearchBar from "../../../../components/searchBar/SearchBar";
+import TabsBar from "../../../../components/tabsBar/TabsBar";
 import "./AccountSettings.scss";
+import Button from "../../../../components/button/Button";
+
+import InputFormSetting from "../../../../components/setttingsComp/inputFormSetting/InputFormSetting";
+
+import Avatar from "@mui/material/Avatar";
+import { deepOrange } from "@mui/material/colors";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { useState } from "react";
 
 export default function AccountSettings() {
-    return (
-        <>
-            <h1>Cuenta</h1>
-        </>
-    )
+  const handleAccount = (e) => {
+    e.preventDefault();
+
+    console.log(e.target);
+  };
+
+  const [user, setUser] = useState({
+    name: "John Doe",
+    school: "IES El Rincon",
+    course: "1º Bachillerato",
+    email: "hola@gmail.com",
+    edad: 14,
+    contrasenia: "1234",
+  });
+
+  return (
+    <div id="page-account-client">
+      <SearchBar />
+      <main id="content-account-client">
+        <section className="container-back">
+          <FaArrowLeftLong />
+        </section>
+        <section className="container-info">
+          <div className="container-img-profile">
+            <Avatar
+              alt={user.name}
+              src="/static/images/avatar/1.jpg"
+              sx={{ bgcolor: deepOrange[500], width: 70, height: 70 }}
+            />
+          </div>
+          <div className="container-info-student">
+            <h1>{user.name}</h1>
+            <h5>{`${user.course}, ${user.school}`}</h5>
+          </div>
+        </section>
+        <form onSubmit={handleAccount} className="container-inputs">
+          <InputFormSetting
+            title={"Nombre"}
+            option={2}
+            placeholder={user.name}
+          />
+          <InputFormSetting
+             title={"Email"}
+             option={2}
+             placeholder={user.email}
+          />
+          <InputFormSetting 
+          title={"Edad"} 
+          option={2} 
+          placeholder={user.edad} 
+          />
+          <InputFormSetting
+            title={"Contraseña"}
+            option={2}
+            placeholder={user.contrasenia}
+          />
+          <InputFormSetting
+            title={"Curso"}
+            option={2}
+            placeholder={user.course}
+          />
+          <div className="container-btn-account">
+            <Button text={"Actualizar"} submit={true} />
+          </div>
+        </form>
+      </main>
+      <TabsBar />
+    </div>
+  );
 }
