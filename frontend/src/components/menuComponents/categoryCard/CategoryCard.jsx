0f2cdc886975a5a0dc243b59610aaee7a251bc84
Isaac Ramos <isaacantonioramosperez@alumno.ieselrincon.es>
2024-12-02 20:12:28 +0000

    categories cambiadas

--- frontend/src/components/menuComponents/categoryCard/CategoryCard.jsx
+++ frontend/src/components/menuComponents/categoryCard/CategoryCard.jsx
@@ -1,15 +1,25 @@
 import "./CategoryCard.scss";
-
+import { Link } from "react-router-dom";
 function CategoryCard({ title, img, count }) {
   return (
     <div className="container-card-category">
-      <div className="container-content-card-category">
-        <h4 className="title-category-card" >{title}</h4>
-        <p className="count-category-card" >{count} {title} </p>
+      {/* <div className="container-content-card-category">
+        <h4 className="title-category-card">{title}</h4>
+        <p className="count-category-card">
+          {count} {title}{" "}
+        </p>
       </div>
-      <a className="container-img-category-card" href="#">
+      <Link className="container-img-category-card" to={"www.google.com"}>
         <img src={img} alt="Img card category" />
-      </a>
+      </Link> */}
+      <Link>
+        <div className="container-content-card-category">
+          <h4 className="title-category-card">{title}</h4>
+          <p className="count-category-card">
+            {count} {title}{" "}
+          </p>
+        </div>
+      </Link>
     </div>
   );
 }
