1af1805acb70c6e269dec11acc6562c0eb650a9b
Mansour <mansourlol440@gmail.com>
2025-01-15 16:19:40 +0000

    Fix deleted js from public, server updated and minor fixes on index models

--- SSR/models/index.js
+++ SSR/models/index.js
@@ -1,6 +1,5 @@
-const Sequelize = require('sequelize');
-const dbConfig = require("../config/db.config.js")
-
+const Sequelize = require("sequelize");
+const dbConfig = require("../config/db.config.js");
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
@@ -11,12 +10,13 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     min: dbConfig.pool.min,
     acquire: dbConfig.pool.acquire,
     idle: dbConfig.pool.idle,
-  }
+  },
 });
 
 const db = {};
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sessionStore = new SequelizeStore({
   db: sequelize,
@@ -24,6 +24,7 @@ const sessionStore = new SequelizeStore({
 
 db.sessionStore = sessionStore;
 db.session = session;
+
 db.sequelize = sequelize;
 
 db.admins = require("./admin.model.js")(sequelize);
@@ -39,32 +40,43 @@ db.coffeShop = require("./coffeShop.model.js")(sequelize);
 // db.inventory = require("./inventory.model.js")(sequelize);
 // db.orderLine = require("./orderLine.model.js")(sequelize);
 
-//! FK 
+//! FK
 
 //* TABLE STUDENT
-/** ID - COURSE */ db.student.belongsTo(db.course, {foreingKey: 'courseId'});
+/** ID - COURSE */ db.student.belongsTo(db.course, { foreingKey: "courseId" });
 
 //* TABLE WALLET
-/** ID - STUDENT */ db.wallet.belongsTo(db.student, {onDelete: 'cascade', foreingKey: 'studentId'});
+/** ID - STUDENT */ db.wallet.belongsTo(db.student, {
+  onDelete: "cascade",
+  foreingKey: "studentId",
+});
 
 //* TABLE ORDER
-/** ID - STUDENT */ db.order.belongsTo(db.student, {foreingKey: 'studentId'});
-/** ID - PRODUCT */ db.order.belongsTo(db.product, {foreingKey: 'productId'});
+/** ID - STUDENT */ db.order.belongsTo(db.student, { foreingKey: "studentId" });
+/** ID - PRODUCT */ db.order.belongsTo(db.product, { foreingKey: "productId" });
 
 //* TABLE PRODUCT
-/** ID - PRODUCT */ db.product.belongsTo(db.categories, {onDelete: 'cascade', foreingKey: 'categoryId'});
+/** ID - PRODUCT */ db.product.belongsTo(db.categories, {
+  onDelete: "cascade",
+  foreingKey: "categoryId",
+});
 
 //* TABLE COFFE SHOP
-/** ID - COFFE SHOP */ db.worker.belongsTo(db.coffeShop, {foreingKey: 'coffeShop'});
+/** ID - COFFE SHOP */ db.worker.belongsTo(db.coffeShop, {
+  foreingKey: "coffeShop",
+});
 
-/** ID - COFFE SHOP */ db.school.belongsTo(db.coffeShop, {foreingKey: 'coffeShop'});
+/** ID - COFFE SHOP */ db.school.belongsTo(db.coffeShop, {
+  foreingKey: "coffeShop",
+});
 
-/** ID - COFFE SHOP */ db.product.belongsTo(db.coffeShop, {foreingKey: 'coffeShop'});
+/** ID - COFFE SHOP */ db.product.belongsTo(db.coffeShop, {
+  foreingKey: "coffeShop",
+});
 
 //* TABLE ADMIN
 /** ID - ADMIN */
-db.coffeShop.belongsTo(db.admins, {foreingKey: 'admin'});
-
+db.coffeShop.belongsTo(db.admins, { foreingKey: "admin" });
 
 // // TABLE WORKER
 // db.worker.hasMany(db.coffeShop, {foreingKey: 'coffeShopId'});
@@ -80,6 +92,5 @@ db.coffeShop.belongsTo(db.admins, {foreingKey: 'admin'});
 // db.school.belongsTo(db.admins, {foreingKey: 'adminId'});
 
 // // TABLE COFFE SHOP
-
 
 module.exports = db;
