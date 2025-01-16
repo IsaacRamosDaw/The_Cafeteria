5cc35ea3d37786c5988ce7f888bff98d3f318ff0
IsaacRamosDaw <isaacantonioramosperez@alumno.ieselrincon.es>
2025-01-16 23:28:00 +0100

    crud products completed

--- SSR/public/js/product/product-delete.js
+++ SSR/public/js/product/product-delete.js
@@ -1,25 +1,24 @@
-const endpoint = '/api/products/'
-const buttons = document.querySelectorAll('.delete-button');
+ const endpoint = '/api/products/'
+    const buttons = document.querySelectorAll('.delete-button');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('.delete-button');
-  buttons.forEach((button) => {
-    alert("hola");
-    button.addEventListener('click', (e) => {
-      const id = e.target.getAttribute('data-id');
-      console.log(endpoint + id);
-      fetch(endpoint + id, {
-        method: 'DELETE',
-      })
-        .then((res) => {
-          if (!res.ok) {
-            console.log("error al eliminar el producto");
-          }
-          e.target.parentElement.remove();
-        })
-        .catch((err) => {
-          console.log("ha habido un problema en la red" + err.message);
+    document.addEventListener('DOMContentLoaded', () => {
+      const buttons = document.querySelectorAll('.delete-button');
+      buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+          const id = e.target.getAttribute('data-id');
+          console.log(endpoint + id);
+          fetch(endpoint + id, {
+              method: 'DELETE',
+            })
+            .then((res) => {
+              if (!res.ok) {
+                console.log("error al eliminar el producto");
+              }
+              e.target.parentElement.remove();
+            })
+            .catch((err) => {
+              console.log("ha habido un problema en la red" + err.message);
+            });
         });
+      });
     });
-  });
-});
