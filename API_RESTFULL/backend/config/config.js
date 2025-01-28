4b2573d335fd0171ffba37ac82bf54a3fb95371e
Mansour <97735675+mansulol@users.noreply.github.com>
2025-01-28 19:17:02 +0000

    Revert "Migrations, seeders and scripts"

--- API_RESTFULL/backend/config/config.js
+++ API_RESTFULL/backend/config/config.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 
-const config = {
+module.exports = {
   development: {
-    username: process.env.MYSQL_USER_DEV,
-    password: process.env.MYSQL_PASSWORD_DEV,
-    database: process.env.MYSQL_DATABASE_DEV,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     host: process.env.DB_HOST,
     dialect: 'mysql'
   },
   test: {
-    username: process.env.MYSQL_USER_TEST,
-    password: process.env.MYSQL_PASSWORD_TEST,
-    database: process.env.MYSQL_DATABASE_TEST,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     host: process.env.DB_HOST,
     dialect: 'mysql'
   },
@@ -22,8 +22,4 @@ const config = {
     host: process.env.DB_HOST,
     dialect: 'mysql'
   }
-};
-
-const env = process.env.NODE_ENV || 'development'
-
-module.exports = config[env]
+}
--- API_RESTFULL/backend/config/db.config.js
+++ API_RESTFULL/backend/config/db.config.js
@@ -1,15 +1,13 @@
-const envConfig = require("./config");
-
 module.exports = {
-  HOST: envConfig.host,
-  USER: envConfig.username,
-  PASSWORD: envConfig.password,
-  DB: envConfig.database,
-  dialect: envConfig.dialect,
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "1234",
+  DB: "db_coffeshop",
+  dialect: "mysql",
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000,
-  },
-};
+    idle: 10000
+  }
+}
