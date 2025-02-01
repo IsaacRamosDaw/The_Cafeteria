181d739cfff00b4ee0ff73d7278f62b39e5b0238
IsaacRamosDaw <isaacantonioramosperez@alumno.ieselrincon.es>
2025-02-01 21:01:25 +0100

    preparando mi propio websocket

--- API_RESTFULL/backend/index.js
+++ API_RESTFULL/backend/index.js
@@ -114,11 +114,19 @@ const usersToSend = [];
 const users = [];
 const helps = [];
 
+// Este código estaba al final del todo pero me parecía que tenía más lógica arriba
+function sendMessage(message) {
+  users.forEach((user) => {
+    user.ws.send(JSON.stringify(message));
+  })
+}
+
 server.on('connection', (ws, incoming_request) => {
   const u = { username: incoming_request.url.split("=")[1]};
   ws.username = u.username;
   const userRef = { ws };
-  usersToSend(u);
+  usersToSend.push(userRef)
+  users.push(u);
   console.log("create");
   console.log(users);
 
@@ -133,11 +141,11 @@ server.on('connection', (ws, incoming_request) => {
 
       // Checking if the message is a valid one
       if (typeof data.type !== 'string') {
-        console.error('Invalid message');
+        console.error('Invalid message, is not string message');
         return;
       }
 
-      if(data.type == 'next please'){
+      if(data.type == 'ask for help'){
         helps.push({
           helper: data.helper,
           helped: data.helped,
@@ -169,11 +177,5 @@ server.on('connection', (ws, incoming_request) => {
     console.log(users);
   });
 
-  //*Falta hacer los ajustes en .env 
 });
 
-function sendMessage(message) {
-  users.forEach((user) => {
-    user.ws.send(JSON.stringify(message));
-  })
-}
